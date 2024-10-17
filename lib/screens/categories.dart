fc8d12a4eafacebcc2e2811f1793943268e796ec
andremoss800 <andremoss800@gmail.com>
2024-10-17 16:35:51 +0300

    made the meals screen and worked on navigation a bit

---
+++ lib/screens/categories.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+import 'package:meals/models/category.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((element) => element.categories.contains(category.id))
+        .toList();
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) =>
+                MealsScreen(title: category.title, meals: filteredMeals)));
+    //gia navigate se allo screen, mporousame na kanoume epishs(to idio einai) Navigator.of(context).push(route)
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          //analogia platous/ipsous
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories) //alternative to .map
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+        ],
+      ),
+      //i can set the number of columns which i wanna have basically
+      //to main tha itan gia na orisw tis grammes
+    );
+  }
+}
